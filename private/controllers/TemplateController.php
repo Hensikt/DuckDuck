d5e7a631384a104fce78746c9e26f426e3ba088e
pk1260 <18251@ma-web.nl>
2020-01-23 11:35:12 +0100

    Push-pas-v1

---
+++ private/controllers/TemplateController.php
@@ -0,0 +1,20 @@
+<?php
+
+class TemplateController
+{
+    function templatepage()
+    {
+        //Hier gaan wij de website in elkaar plakken
+        //Models hier boven incuden met een variable declaratie : $variable-name = model-function()
+
+        //Header heeft de nav en pagina locatie
+        include __DIR__ . '/../views/header.php';
+
+        //Hier komt julie deel van de website plak op deze plek = '/../file.php';
+        include __DIR__ . '/../views/blog.php';
+
+        //Hier komt de footer met javascripts en andere stuff
+        include __DIR__ . '/../views/footer.php';
+    }
+}
+?>
